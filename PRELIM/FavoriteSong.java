6262b458bb949c25b3cb802e40499105af19847c
Evan Batac <133208566+EvanBatac@users.noreply.github.com>
2023-10-21 16:21:35 +0800

    Create FavoriteSong.java

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong{
+   public static void main(String[] args){
+   
+     System.out.println("Now I've got you in my space");
+     System.out.println("I won't let go of you");
+     System.out.println("Got you shackled in my embrace");
+     System.out.println("I'm latching on to you");
+
+   
+   }
+}
